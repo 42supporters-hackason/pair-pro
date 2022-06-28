cae762c04cbe464cd2acffde01ca46c69be1ce55
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-06-28 14:20:37 +0900

    枠組みの作成

--- client/src/pages/client/chat.tsx
+++ client/src/pages/client/chat.tsx
@@ -1,30 +1,42 @@
-import {
-  Box,
-  Grid,
-  Input,
-  TextareaAutosize,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, TextareaAutosize, Typography } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import SendRoundedIcon from "@mui/icons-material/SendRounded";
 import React from "react";
+import { Card } from "../../components/Card";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 
 /**
  * p2p相手とやり取りをするページ
  */
 export const ChatPage = () => {
   return (
-    <Box>
-      <Box sx={{ width: "70%", float: "left" }}>
-        ビデオチャット機能を追加する予定
+    <Box sx={{ display: "flex" }}>
+      <Box sx={{ width: "70%", height: "calc(100vh - 68.5px)", m: 3 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            height: "calc(100% - 68.5px)",
+            width: "100%",
+          }}
+        >
+          <Box sx={{ display: "flex", height: "80%", width: "100%", gap: 3 }}>
+            <Box sx={{ width: "50%" }}>
+              <Card>自分</Card>
+            </Box>
+            <Box sx={{ width: "50%" }}>
+              <Card>相手</Card>
+            </Box>
+          </Box>
+          <Box></Box>
+        </Box>
       </Box>
       <Box
         sx={{
           width: "30%",
           display: "flex",
           flexDirection: "column",
-          float: "left",
           height: "calc(100vh - 68.5px)",
         }}
       >
@@ -41,13 +53,15 @@ export const ChatPage = () => {
           <Typography fontWeight="bold">タイトル</Typography>
           <CloseIcon sx={{ mr: "10px", cursor: "pointer" }} />
         </Box>
-        <Box>チャット</Box>
+        <Box sx={{ m: "15px" }}>チャット</Box>
         <Box
           sx={{
             width: "100%",
             display: "flex",
+            alignItems: "center",
             gap: "15px",
             mt: "auto",
+            mb: "10px",
           }}
         >
           <TextareaAutosize
@@ -60,7 +74,7 @@ export const ChatPage = () => {
               padding: "15px",
             }}
           />
-          <SendRoundedIcon sx={{ cursor: "pointer", mb: "10px" }} />
+          <SendRoundedIcon sx={{ cursor: "pointer", mb: "10px", mt: "auto" }} />
         </Box>
       </Box>
     </Box>
