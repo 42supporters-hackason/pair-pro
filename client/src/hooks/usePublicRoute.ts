d9d5d0d84815f6fc36e7e8021dd32e0be98f6bdb
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-07-02 23:35:43 +0900

    authの処理

---
+++ client/src/hooks/usePublicRoute.ts
@@ -0,0 +1,19 @@
+import { PUBLIC_ROUTE_PATH } from "./../routes/public";
+import { useCallback } from "react";
+import { NavigateOptions, useNavigate } from "react-router-dom";
+
+/**
+ * 権限不必要画面の画面遷移Hooks
+ */
+export const usePublicRoute = () => {
+  const navigate = useNavigate();
+
+  const goToLogin = useCallback(
+    (options?: NavigateOptions) => {
+      navigate(PUBLIC_ROUTE_PATH.LOGIN, options);
+    },
+    [navigate]
+  );
+
+  return { goToLogin };
+};
--- client/src/pages/components/ClientLayout.tsx
+++ client/src/pages/components/ClientLayout.tsx
@@ -1,12 +1,27 @@
 import { Box } from "@mui/material";
-import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Suspense, useEffect } from "react";
+import { Outlet, useSearchParams } from "react-router-dom";
 import { GeneralHeader } from "../../components/GeneralHeader";
+import { usePublicRoute } from "../../hooks/usePublicRoute";
+import { accessTokenStorage } from "../../utils/local-storage/access_token";
 
 /**
  * Client画面共通のLayout
  */
 export const ClientLayout = () => {
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get("code");
+  const { goToLogin } = usePublicRoute();
+
+  useEffect(() => {
+    if (code && accessTokenStorage.load() === null) {
+      accessTokenStorage.save(code);
+    }
+    if (accessTokenStorage.load() === null) {
+      goToLogin({ replace: true });
+    }
+  }, []);
+
   return (
     <Box
       sx={{
