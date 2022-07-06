13a36d5ca7238633370f321eadbc57a7fbb0b97e
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-07-06 23:10:51 +0900

    作成

--- api/src/graphql/Skill.ts
+++ api/src/graphql/Skill.ts
@@ -1,9 +1,31 @@
-import { extendType, objectType } from "nexus"
+import { extendType, objectType } from "nexus";
+import json from "../assets/json/languages.json";
 
 export const Skill = objectType({
   name: "Skill",
   definition(t) {
     t.nonNull.int("id");
     t.nonNull.string("name");
-  }
-})
+  },
+});
+
+/**
+ * 全ての言語名を持ったオブジェクトの配列
+ * idはjsonの順番でそれぞれindexを付与
+ */
+const languagesObject = json.languages.map((language, index) => ({
+  id: index,
+  name: language,
+}));
+
+export const SkillQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.nonNull.list.nonNull.field("skills", {
+      type: "Skill",
+      resolve(parent, args, context, info) {
+        return languagesObject;
+      },
+    });
+  },
+});
