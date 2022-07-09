c71c695117a0f04ace39d9f16b0fdae43b52bc37
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-07-09 10:29:31 +0900

    profileの修正

--- client/src/components/ProfileCard/index.tsx
+++ client/src/components/ProfileCard/index.tsx
@@ -71,17 +71,12 @@ export const ProfileCard = ({
             <Box
               component="img"
               sx={{ height: "150px" }}
-              src={`https://github-readme-stats.vercel.app/api?username=${githubId}&theme=onedark&show_icons=true)](https://github.com/anuraghazra/github-readme-stats`}
+              src={`https://github-readme-stats.vercel.app/api?username=${githubId}&theme=onedark&show_icons=true`}
             />
             <Box
               component="img"
               sx={{ height: "150px" }}
-              src={`https://raw.githubusercontent.com/${githubId}/${githubId}/main/profile-summary-card-output/default/1-repos-per-language.svg`}
-            />
-            <Box
-              component="img"
-              sx={{ height: "150px" }}
-              src={`https://raw.githubusercontent.com/${githubId}/${githubId}/main/profile-summary-card-output/default/3-stats.svg`}
+              src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${githubId}&layout=compact`}
             />
           </Box>
           <Box
