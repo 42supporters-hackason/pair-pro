397efe884f8a4c5b560b2edc10117ac3ee243812
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-07-10 14:11:08 +0900

    fix

--- client/src/gen/graphql-client.ts
+++ client/src/gen/graphql-client.ts
@@ -1,10 +1,16 @@
-import { gql } from '@apollo/client';
-import * as Apollo from '@apollo/client';
+import { gql } from "@apollo/client";
+import * as Apollo from "@apollo/client";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -16,50 +22,47 @@ export type Scalars = {
 };
 
 export type AuthPayLoad = {
-  __typename?: 'AuthPayLoad';
-  token: Scalars['String'];
+  __typename?: "AuthPayLoad";
+  token: Scalars["String"];
   user: User;
 };
 
 export type Message = {
-  __typename?: 'Message';
-  content: Scalars['String'];
-  id: Scalars['Int'];
+  __typename?: "Message";
+  content: Scalars["String"];
+  id: Scalars["Int"];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: "Mutation";
   authGithub: AuthPayLoad;
   post: Post;
   updateMe?: Maybe<User>;
 };
 
-
 export type MutationAuthGithubArgs = {
-  code: Scalars['String'];
+  code: Scalars["String"];
 };
-
 
 export type MutationPostArgs = {
-  description: Scalars['String'];
-  title: Scalars['String'];
+  description: Scalars["String"];
+  title: Scalars["String"];
 };
 
-
 export type MutationUpdateMeArgs = {
-  bio?: InputMaybe<Scalars['String']>;
-  name?: InputMaybe<Scalars['String']>;
+  bio?: InputMaybe<Scalars["String"]>;
+  name?: InputMaybe<Scalars["String"]>;
 };
 
 export type Post = {
-  __typename?: 'Post';
-  description: Scalars['String'];
-  id: Scalars['Int'];
-  title: Scalars['String'];
+  __typename?: "Post";
+  description: Scalars["String"];
+  id: Scalars["Int"];
+  title: Scalars["String"];
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   feed: Array<Post>;
   me: User;
   skills: Array<Skill>;
@@ -67,60 +70,76 @@ export type Query = {
   users: Array<User>;
 };
 
-
 export type QueryUserArgs = {
-  id: Scalars['Int'];
+  id: Scalars["Int"];
 };
 
 export type Skill = {
-  __typename?: 'Skill';
-  id: Scalars['Int'];
-  name: Scalars['String'];
+  __typename?: "Skill";
+  id: Scalars["Int"];
+  name: Scalars["String"];
 };
 
 export type User = {
-  __typename?: 'User';
-  bio: Scalars['String'];
+  __typename?: "User";
+  bio: Scalars["String"];
   driverPost: Array<Post>;
-  githubId: Scalars['String'];
-  githubLogin: Scalars['String'];
-  id: Scalars['Int'];
-  matchingPoint: Scalars['Int'];
-  name: Scalars['String'];
+  githubId: Scalars["String"];
+  githubLogin: Scalars["String"];
+  id: Scalars["Int"];
+  matchingPoint: Scalars["Int"];
+  name: Scalars["String"];
   navigatorPost: Array<Post>;
 };
 
 export type SignInMutationVariables = Exact<{
-  code: Scalars['String'];
+  code: Scalars["String"];
 }>;
 
-
-export type SignInMutation = { __typename?: 'Mutation', authGithub: { __typename?: 'AuthPayLoad', token: string, user: { __typename?: 'User', id: number, name: string, githubLogin: string, matchingPoint: number, bio: string } } };
+export type SignInMutation = {
+  __typename?: "Mutation";
+  authGithub: {
+    __typename?: "AuthPayLoad";
+    token: string;
+    user: {
+      __typename?: "User";
+      id: number;
+      name: string;
+      githubLogin: string;
+      matchingPoint: number;
+      bio: string;
+    };
+  };
+};
 
 export type UpdateProfileMutationVariables = Exact<{
-  name: Scalars['String'];
-  bio: Scalars['String'];
+  name: Scalars["String"];
+  bio: Scalars["String"];
 }>;
 
-
-export type UpdateProfileMutation = { __typename?: 'Mutation', updateMe?: { __typename?: 'User', name: string, bio: string } | null };
-
+export type UpdateProfileMutation = {
+  __typename?: "Mutation";
+  updateMe?: { __typename?: "User"; name: string; bio: string } | null;
+};
 
 export const SignInDocument = gql`
-    mutation SignIn($code: String!) {
-  authGithub(code: $code) {
-    token
-    user {
-      id
-      name
-      githubLogin
-      matchingPoint
-      bio
+  mutation SignIn($code: String!) {
+    authGithub(code: $code) {
+      token
+      user {
+        id
+        name
+        githubLogin
+        matchingPoint
+        bio
+      }
     }
   }
-}
-    `;
-export type SignInMutationFn = Apollo.MutationFunction<SignInMutation, SignInMutationVariables>;
+`;
+export type SignInMutationFn = Apollo.MutationFunction<
+  SignInMutation,
+  SignInMutationVariables
+>;
 
 /**
  * __useSignInMutation__
@@ -139,22 +158,36 @@ export type SignInMutationFn = Apollo.MutationFunction<SignInMutation, SignInMut
  *   },
  * });
  */
-export function useSignInMutation(baseOptions?: Apollo.MutationHookOptions<SignInMutation, SignInMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<SignInMutation, SignInMutationVariables>(SignInDocument, options);
-      }
+export function useSignInMutation(
+  baseOptions?: Apollo.MutationHookOptions<
+    SignInMutation,
+    SignInMutationVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useMutation<SignInMutation, SignInMutationVariables>(
+    SignInDocument,
+    options
+  );
+}
 export type SignInMutationHookResult = ReturnType<typeof useSignInMutation>;
 export type SignInMutationResult = Apollo.MutationResult<SignInMutation>;
-export type SignInMutationOptions = Apollo.BaseMutationOptions<SignInMutation, SignInMutationVariables>;
+export type SignInMutationOptions = Apollo.BaseMutationOptions<
+  SignInMutation,
+  SignInMutationVariables
+>;
 export const UpdateProfileDocument = gql`
-    mutation UpdateProfile($name: String!, $bio: String!) {
-  updateMe(name: $name, bio: $bio) {
-    name
-    bio
+  mutation UpdateProfile($name: String!, $bio: String!) {
+    updateMe(name: $name, bio: $bio) {
+      name
+      bio
+    }
   }
-}
-    `;
-export type UpdateProfileMutationFn = Apollo.MutationFunction<UpdateProfileMutation, UpdateProfileMutationVariables>;
+`;
+export type UpdateProfileMutationFn = Apollo.MutationFunction<
+  UpdateProfileMutation,
+  UpdateProfileMutationVariables
+>;
 
 /**
  * __useUpdateProfileMutation__
@@ -174,10 +207,24 @@ export type UpdateProfileMutationFn = Apollo.MutationFunction<UpdateProfileMutat
  *   },
  * });
  */
-export function useUpdateProfileMutation(baseOptions?: Apollo.MutationHookOptions<UpdateProfileMutation, UpdateProfileMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<UpdateProfileMutation, UpdateProfileMutationVariables>(UpdateProfileDocument, options);
-      }
-export type UpdateProfileMutationHookResult = ReturnType<typeof useUpdateProfileMutation>;
-export type UpdateProfileMutationResult = Apollo.MutationResult<UpdateProfileMutation>;
-export type UpdateProfileMutationOptions = Apollo.BaseMutationOptions<UpdateProfileMutation, UpdateProfileMutationVariables>;
+export function useUpdateProfileMutation(
+  baseOptions?: Apollo.MutationHookOptions<
+    UpdateProfileMutation,
+    UpdateProfileMutationVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useMutation<
+    UpdateProfileMutation,
+    UpdateProfileMutationVariables
+  >(UpdateProfileDocument, options);
+}
+export type UpdateProfileMutationHookResult = ReturnType<
+  typeof useUpdateProfileMutation
+>;
+export type UpdateProfileMutationResult =
+  Apollo.MutationResult<UpdateProfileMutation>;
+export type UpdateProfileMutationOptions = Apollo.BaseMutationOptions<
+  UpdateProfileMutation,
+  UpdateProfileMutationVariables
+>;
