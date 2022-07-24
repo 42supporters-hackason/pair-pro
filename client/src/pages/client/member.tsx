d16abe7574d91e766af3aab9fa1827237fe78a0c
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-07-24 13:35:21 +0900

    userItemsの作成

--- client/src/pages/client/member.tsx
+++ client/src/pages/client/member.tsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
+/**
+ * client/member
+ */
 export const MemberPage = () => {
-  return <Box></Box>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        mt: "30px",
+      }}
+    >
+      <Typography variant="h6" fontWeight="bold">
+        42tokyo
+      </Typography>
+    </Box>
+  );
 };
