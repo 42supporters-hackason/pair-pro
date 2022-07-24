9d7762347b1300a9bb9cd24863da3f9e3249ea97
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-07-24 11:33:29 +0900

    createCommunity画面の修正

--- client/src/pages/public/create/community.tsx
+++ client/src/pages/public/create/community.tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Box, Button, TextField, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { useClientRoute } from "../../../hooks/useClientRoute";
 import { usePublicRoute } from "../../../hooks/usePublicRoute";
+import {
+  CreateCommunitySchema,
+  createCommunitySchema,
+} from "../../validation/create_community_validation";
 
 export const CreateCommunityPage = () => {
+  /**
+   * misc.
+   */
   const { goToCommunity } = usePublicRoute();
+  const { goToHome } = useClientRoute();
+
+  /**
+   * form validation
+   */
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateCommunitySchema>({
+    resolver: zodResolver(createCommunitySchema),
+  });
+
+  /**
+   * event-handler
+   */
+  const handleCreateCommunity = useCallback(() => {
+    goToHome({ replace: true });
+  }, [goToHome]);
+
   return (
     <Box
       sx={{
@@ -13,6 +43,8 @@ export const CreateCommunityPage = () => {
         flexDirection: "column",
         justifyContent: "center",
       }}
+      component="form"
+      onSubmit={handleSubmit(handleCreateCommunity)}
     >
       <Box
         sx={{
@@ -24,7 +56,12 @@ export const CreateCommunityPage = () => {
         <Typography variant="h6" fontWeight="bold">
           新しいcomunityの名前を入力してください
         </Typography>
-        <TextField label="community名" sx={{ mt: "45px", width: "450px" }} />
+        <TextField
+          label="community名"
+          sx={{ mt: "45px", width: "450px" }}
+          {...register("communityName")}
+        />
+        <Typography color="error">{errors.communityName?.message}</Typography>
         <Button
           variant="contained"
           sx={{
@@ -34,6 +71,7 @@ export const CreateCommunityPage = () => {
             borderRadius: "20px",
             fontWeight: "bold",
           }}
+          type="submit"
         >
           作成する
         </Button>
