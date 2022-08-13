730427e81a8ac9724aa926c0ea1276278d7cde54
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-08-13 20:51:55 +0900

    add: statistics hooks routes

---
+++ client/src/pages/client/statistics.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box } from "@mui/system";
+
+export const StatisticsPage = () => {
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box></Box>
+    </Box>
+  );
+};
