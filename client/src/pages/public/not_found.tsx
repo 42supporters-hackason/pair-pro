bd9e226f912c692e33f6a47ad03af38c6243fee9
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-08-16 22:41:23 +0900

    404　page

--- client/src/pages/public/not_found.tsx
+++ client/src/pages/public/not_found.tsx
@@ -1,6 +1,47 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useAuth } from "../../context/auth";
+import { useClientRoute } from "../../hooks/useClientRoute";
+import { usePublicRoute } from "../../hooks/usePublicRoute";
 
 export const NotFoundPage = () => {
-  return <Box></Box>;
+  const { loginStatus } = useAuth();
+  const { goToHome } = useClientRoute();
+  const { goToCommunity, goToLogin } = usePublicRoute();
+
+  const handleBackPage = useCallback(() => {
+    if (loginStatus === "authFinished") {
+      goToCommunity({ replace: true });
+    }
+    if (loginStatus === "logined") {
+      goToHome({ replace: true });
+    }
+    goToLogin({ replace: true });
+  }, [goToCommunity, goToHome, goToLogin, loginStatus]);
+
+  return (
+    <Box sx={{ height: "100vh" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          mt: "200px",
+          gap: "50px",
+        }}
+      >
+        <Typography variant="h4" fontWeight="bold">
+          Not Found Page
+        </Typography>
+        <Button
+          variant="contained"
+          sx={{ height: "50px", borderRadius: "40px", fontSize: "20px", px: 3 }}
+          onClick={handleBackPage}
+        >
+          前のページに戻る
+        </Button>
+      </Box>
+    </Box>
+  );
 };
