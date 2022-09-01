faa7afc7c3ec2a84ae90db1bfad5c8a754b7c5af
Takumi Hara <takumihara1226@gmail.com>
2022-09-01 20:53:47 +0900

    fix: order of messagesByPostId

--- api/src/graphql/Message.ts
+++ api/src/graphql/Message.ts
@@ -61,6 +61,7 @@ export const MessageQuery = extendType({
 
         return await context.prisma.message.findMany({
           where: { postId },
+          orderBy: { id: "asc" },
         });
       },
     });
