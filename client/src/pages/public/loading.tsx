e0faeaa0c5d88090da1df1c90418a92f0d737f2f
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-09-03 21:33:05 +0900

    can back page

--- client/src/pages/public/loading.tsx
+++ client/src/pages/public/loading.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { Box, CircularProgress } from "@mui/material";
+import React, { useCallback } from "react";
+import { Box, Button, CircularProgress } from "@mui/material";
+import { useAuth } from "../../context/auth";
+import { useClientRoute } from "../../hooks/useClientRoute";
+import { usePublicRoute } from "../../hooks/usePublicRoute";
 
 /**
  * ローディング画面
  */
 export const LoadingPage = () => {
+  const { loginStatus } = useAuth();
+  const { goToHome } = useClientRoute();
+  const { goToCommunity, goToLogin } = usePublicRoute();
+
+  const handleBackPage = useCallback(() => {
+    if (loginStatus === "authFinished") {
+      goToCommunity({ replace: true });
+    }
+    if (loginStatus === "logined") {
+      goToHome({ replace: true });
+    }
+    goToLogin({ replace: true });
+  }, [goToCommunity, goToHome, goToLogin, loginStatus]);
+
   return (
     <Box
       sx={{
@@ -13,9 +30,18 @@ export const LoadingPage = () => {
         justifyContent: "center",
         alignItems: "center",
         mt: "300px",
+        gap: "50px",
       }}
     >
       <CircularProgress size={100} />
+      <Button
+        variant="contained"
+        color="secondary"
+        sx={{ height: "50px", borderRadius: "40px", fontSize: "20px", px: 3 }}
+        onClick={handleBackPage}
+      >
+        前のページに戻る
+      </Button>
     </Box>
   );
 };
