59cca8df671640be6d60c90017a7fcf454da2657
rnakai <rnakai@student.42tokyo.jp>
2022-09-14 18:03:52 +0900

    テストデータ追加sqlの供養

---
+++ docs/used_seed/addProfile.sql
@@ -0,0 +1,178 @@
+insert into
+  public."Profile"
+values
+  (
+    50,
+    '2022-09-10 08:36:21.659',
+    'gavinfielder',
+    5,
+    '',
+    50,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  );
+
+insert into
+  public."Profile"
+values
+  (
+    51,
+    '2022-09-10 08:36:21.659',
+    'AnaisTry',
+    5,
+    '',
+    51,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    52,
+    '2022-09-10 08:36:21.659',
+    'ViRUS-Trojan',
+    5,
+    '',
+    52,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    53,
+    '2022-09-10 08:36:21.659',
+    'patrisor',
+    5,
+    '',
+    53,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    54,
+    '2022-09-10 08:36:21.659',
+    'eisukeesaki',
+    5,
+    '',
+    54,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    55,
+    '2022-09-10 08:36:21.659',
+    'boularasgc',
+    5,
+    '',
+    55,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    56,
+    '2022-09-10 08:36:21.659',
+    'jan-Lope',
+    5,
+    '',
+    56,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    57,
+    '2022-09-10 08:36:21.659',
+    'jcass77',
+    5,
+    '',
+    57,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    58,
+    '2022-09-10 08:36:21.659',
+    'tkem',
+    5,
+    '',
+    58,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    59,
+    '2022-09-10 08:36:21.659',
+    'sebmarkbage',
+    5,
+    '',
+    59,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    60,
+    '2022-09-10 08:36:21.659',
+    'richardjrossiii',
+    5,
+    '',
+    60,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    61,
+    '2022-09-10 08:36:21.659',
+    'nhurman',
+    5,
+    '',
+    61,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    62,
+    '2022-09-10 08:36:21.659',
+    'JeffreySpehar',
+    5,
+    '',
+    62,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    63,
+    '2022-09-10 08:36:21.659',
+    'bember-usr',
+    5,
+    '',
+    63,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    64,
+    '2022-09-10 08:36:21.659',
+    'acarlson99',
+    5,
+    'Student studying at 42 silicon valley. A fan of C, C++, golang, awk, and regex',
+    64,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    65,
+    '2022-09-10 08:36:21.659',
+    'naamoonoo',
+    5,
+    'Making a differenceStudent studying at 42 silicon valley. A fan of C, C++, golang, awk, and regex',
+    65,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    66,
+    '2022-09-10 08:36:21.659',
+    'qst0',
+    5,
+    'I love learning!Making a differenceStudent studying at 42 silicon valley. A fan of C, C++, golang, awk, and regex',
+    66,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    67,
+    '2022-09-10 08:36:21.659',
+    'kevinfrei',
+    5,
+    'I work at Facebook, managing a group of brilliant people who do performance, reliability, and efficiency stuff for Android, iOS, WWW/PHP, and ML/AI.I love learning!Making a differenceStudent studying at 42 silicon valley. A fan of C, C++, golang, awk, and regex',
+    67,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  ),
+  (
+    68,
+    '2022-09-10 08:36:21.659',
+    'insomniacslk',
+    5,
+    'Andrea BarberioI work at Facebook, managing a group of brilliant people who do performance, reliability, and efficiency stuff for Android, iOS, WWW/PHP, and ML/AI.I love learning!Making a differenceStudent studying at 42 silicon valley. A fan of C, C++, golang, awk, and regex',
+    68,
+    '87bc9e7e-ea95-4cc7-84ea-52343099f231'
+  );
---
+++ docs/used_seed/addUser.sql
@@ -0,0 +1,21 @@
+insert into
+  public."User"
+values
+  (51, '', 'AnaisTry', ''),
+  (52, '', 'ViRUS-Trojan', ''),
+  (53, '', 'patrisor', ''),
+  (54, '', 'eisukeesaki', ''),
+  (55, '', 'boularasgc', ''),
+  (56, '', 'jan-Lope', ''),
+  (57, '', 'jcass77', ''),
+  (58, '', 'tkem', ''),
+  (59, '', 'sebmarkbage', ''),
+  (60, '', 'richardjrossiii', ''),
+  (61, '', 'nhurman', ''),
+  (62, '', 'JeffreySpehar', ''),
+  (63, '', 'bember-usr', ''),
+  (64, '', 'acarlson99', ''),
+  (65, '', 'naamoonoo', ''),
+  (66, '', 'qst0', ''),
+  (67, '', 'kevinfrei', ''),
+  (68, '', 'insomniacslk', '');
